249b0a58fc0073f114c12c56df38320ad807877f
Kamel38 <eelabouti@gmail.com>
2018-09-11 10:03:39 +0200

    Complete authentification system

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   resources :users, except: [:new]
   get 'signup', to: 'users#new'
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   resources :articles 
   # the resources function automatically generates the routes.
   # Exemple : 'ressources :posts' generates this :
